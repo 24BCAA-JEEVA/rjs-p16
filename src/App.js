91ae7aab9478fafaa17d418263c50c04dcf729fd
24BCAA-JEEVA <jeevabca666@gmail.com>
2026-09-23 12:29:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ import Contact from "./Contact";
 function App() {
   return (
     <BrowserRouter>
+    <header>
+      <h1>My React Website</h1>
+      <nav>
       <Navigation />
+      </nav>
+      </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
